19433e1dc1606edf705106400e2b9bb9282b5d9a
BukanTv-Biasa <haidirdn@gmail.com>
2022-01-31 22:08:06 +0900

    Membuat File Object Tentang Javaascript

---
+++ Latihan 15(OBJECT)/script.js
@@ -0,0 +1,11 @@
+var bio = {
+    nama : "Tayo",
+    umur : 12,
+    kelas : "3SD",
+    alamat : {
+        jalan : "Aimas",
+        kota : "Papua Bro",
+        provinsi : "Papua Barat"
+
+    }
+};
